815440c1beb80474cda2f8cc9fda00d6dd17b2c5
Aiden Pearce <mgmdrsl05@gmail.com>
2026-02-21 11:38:55 +0300

    Добавлен бизнес калькулятор доходав меню, чтобы рассчитать примерный доход, и испозьзован fmt.Scan

--- menu.go
+++ menu.go
@@ -11,6 +11,7 @@ func main() {
 		fmt.Println("1. Зарабоок")
 		fmt.Println("2. Проверить кошелек")
 		fmt.Println("3. Рискованные инвестиции")
+		fmt.Println("4. Рассчитать рогноз дохода")
 		fmt.Println("0. Выход")
 		fmt.Print("Выберите действие: ")
 
@@ -31,6 +32,21 @@ func main() {
 			} else {
 				fmt.Println("Недостаточно средств для инвестиции!")
 			}
+		case 4:
+			var palmCount int
+			var harvestPerPalm int
+			var price int
+
+			fmt.Println("Сколько всего пальм в саду? ")
+			fmt.Scan(&palmCount)
+			fmt.Println("Сколько кг фиников даёт одна пальма? ")
+			fmt.Scan(&harvestPerPalm)
+			fmt.Println("Цена за 1 кг фиников: ")
+			fmt.Scan(&price)
+
+			totalProfit := palmCount * harvestPerPalm * price
+			fmt.Printf("Твой прогноз дохода %d руб.\n", totalProfit)
+
 		case 0:
 			fmt.Println("Завершение работы... До свидания!")
 			return // Это слово полностью остановит функцию main и выйдет из программы
