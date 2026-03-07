49ce8a6107ef964e6a120bd82bd22d086d60785c
Aiden Pearce <mgmdrsl05@gmail.com>
2026-03-07 07:02:07 +0300

    Внедрена система аналитики: учет продаж по сортам, выручка с Меджула и сохранение статистики в файл

--- menu.go
+++ menu.go
@@ -8,8 +8,7 @@ import (
 )
 
 func main() {
-
-	balance, skill, gardenHealth, totalSales := loadGame()
+	balance, skill, gardenHealth, totalSales, medjulCount, regularCount, wildCount, medjulMoney := loadGame()
 	rand.Seed(time.Now().UnixNano())
 	showGreetings()
 	marketPrice := 100
@@ -23,6 +22,7 @@ func main() {
 		fmt.Println("3. Рискованные инвестиции")
 		fmt.Println("4. Рассчитать рогноз дохода")
 		fmt.Println("5. Уход за садом")
+		fmt.Println("6. Просмотр статистики")
 		fmt.Println("0. Выход")
 		fmt.Print("Выберите действие: ")
 
@@ -32,7 +32,7 @@ func main() {
 
 		switch choice {
 		case 1:
-			balance, gardenHealth = sellDates(balance, skill, gardenHealth, marketPrice)
+			balance, gardenHealth, medjulCount, regularCount, wildCount, medjulMoney = sellDates(balance, skill, gardenHealth, marketPrice, medjulCount, regularCount, wildCount, medjulMoney)
 			balance = payTaxes(balance, gardenHealth)
 			totalSales++
 		case 2:
@@ -44,8 +44,23 @@ func main() {
 			calculateIncome()
 		case 5:
 			balance, gardenHealth = fertilizeGarden(balance, gardenHealth)
+		case 6:
+			fmt.Println("\n========== СТАТИСТИКА САДА ==========")
+            fmt.Printf("Королевский Меджул:  %d сборов (Выручка: %d руб.)\n", medjulCount, medjulMoney)
+            fmt.Printf("Обычные финики:      %d сборов\n", regularCount)
+            fmt.Printf("Дикие финики (лечебные): %d сборов\n", wildCount)
+            fmt.Println("=====================================")
+
+			if medjulCount> regularCount{
+				fmt.Println("Ваша стратегия: Рискованный Султан!")
+			}else if wildCount> regularCount {
+				fmt.Println("Ваша стратегия: Мудрый Садовник!")
+			} else {
+				fmt.Println("Ваша стратегия: Стабильный Фермер.")
+			}
+			
 		case 0:
-			saveGame(balance, skill, gardenHealth, totalSales)
+			saveGame(balance, skill, gardenHealth, totalSales, medjulCount, regularCount, wildCount, medjulMoney)
 			fmt.Println("Завершение работы... До свидания!")
 			return // Это слово полностью остановит функцию main и выйдет из программы
 		default:
@@ -58,24 +73,24 @@ func showGreetings() {
 	fmt.Println("Добро пожаловать в систеу управления садом, Амир!")
 }
 
-func loadGame() (int, int, int, int) {
+func loadGame() (int, int, int, int, int, int, int, int) {
 	// Читаем файл
 	data, err := os.ReadFile("save.txt")
 
 	// Если файла нет (первый запуск), возвращаем стандартные значения
 	if err != nil {
-		return 0, 1, 100, 0
+		return 0, 1, 100, 0,0,0,0,0
 	}
 
-	var b, s, h, t int
+	var b, s, h, t, mc, rc, wc, mm int
 	// Вытаскиваем числа из прочитанных данных
-	fmt.Sscanf(string(data), "%d %d %d %d", &b, &s, &h, &t)
+	fmt.Sscanf(string(data), "%d %d %d %d %d %d %d %d", &b, &s, &h, &t, &mc, &rc, &wc, &mm)
 
 	fmt.Println(">>> Прогресс успешно загружен!")
-	return b, s, h, t
+	return b, s, h, t, mc, rc, wc, mm
 }
 
-func sellDates(balance int, skill int, health int, price int) (int, int) {
+func sellDates(balance, skill, health, price, medjulC, regC, wildC,medjulM int) (int, int, int, int, int, int) {
 	fmt.Println("\n--- ВЫБОР СОРТА ДЛЯ СБОРА ---")
 	fmt.Println("1. Обычные (Стандартный дохо, износ -10%)")
 	fmt.Println("2. Меджул (Доход х3, износ -35%!)")
@@ -91,14 +106,18 @@ func sellDates(balance int, skill int, health int, price int) (int, int) {
 	switch cropChoice {
 	case 1:
 		fmt.Println(">>>Сщбираем обычные финики...")
+		regC++
 	case 2:
 		fmt.Println(">>>РИСК! Собираем элитный Меджул...")
 		profit = profit * 3
 		wear = 35
+		medjulC++
+		medjulM += profit
 	case 3:
 		fmt.Println(">>> Забота о саде: собираем дикие финики...")
 		profit = 10
 		wear = -5
+		wildC++
 	default:
 		fmt.Println("Неверный выбор собираем как обычно.")
 	}
@@ -126,7 +145,7 @@ func sellDates(balance int, skill int, health int, price int) (int, int) {
 	if newHealth < 0   {newHealth = 0}
 
 	fmt.Printf(">>> Итог: Доход %d руб. Здоровье сада теперь: %d%%\n", profit, newHealth)
-	return balance + profit, newHealth
+	return balance + profit, newHealth, medjulC, regC, wildC, medjulM
 }
 
 func printBalance(currentBalance int) {
@@ -181,9 +200,9 @@ func fertilizeGarden(balance int, health int) (int, int) {
 	}
 }
 
-func saveGame(balance int, skill int, health int, total int) {
+func saveGame(balance, skill, health, total, medjulC, regC, wildC, medjulM int) {
 	//создаем строку и наших данных
-	data := fmt.Sprintf("%d %d %d %d", balance, skill, health, total)
+	data := fmt.Sprintf("%d %d %d %d %d %d %d %d", balance, skill, health, total, medjulC, regC, wildC, medjulM)
 	//записываем ее в текстовы файл
 	//0644 - это права доступа стандарт для файлов
 	err := os.WriteFile("save.txt", []byte(data), 0644)
